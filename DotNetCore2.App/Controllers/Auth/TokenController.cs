acd69e91f864ca63a46eb0866b0299f0488331dd
Kmillssd <Kmillssoftwareengineer@gmail.com>
2018-01-13 14:07:49 +0000

    Additional checks on refreshing token.

--- DotNetCore2.App/Controllers/Auth/TokenController.cs
+++ DotNetCore2.App/Controllers/Auth/TokenController.cs
@@ -19,13 +19,15 @@ namespace DotNetCore2.Controllers.Auth
     [Route("api/token")]
     public class TokenController : Controller
     {
-        private readonly ICoreGetAllQuery<CoreUser> _userQuery;
-        private readonly IOptions<Audience> _settings;
+        private readonly ICoreGetAllQuery<CoreUser> _userGetAllQuery;
+        private readonly ICoreGetByIdQuery<CoreUser> _userGetByIdQuery;
+        private readonly IOptions<Audience> _authSettings;
 
-        public TokenController(ICoreGetAllQuery<CoreUser> userQuery, IOptions<Audience> settings)
+        public TokenController(ICoreGetAllQuery<CoreUser> userGetAllQuery, ICoreGetByIdQuery<CoreUser> userGetByIdQuery, IOptions<Audience> authSettings)
         {
-            _userQuery = userQuery;
-            _settings = settings;
+            _userGetAllQuery = userGetAllQuery;
+            _userGetByIdQuery = userGetByIdQuery;
+            _authSettings = authSettings;
         }
 
         [HttpGet()]
@@ -42,16 +44,19 @@ namespace DotNetCore2.Controllers.Auth
         {
             var decodedExpiredToken = new JwtSecurityToken(expiredToken);
 
-            if (decodedExpiredToken.Issuer != _settings.Value.Iss)
-                return BadRequest("Invalid iss");
-
             //additional checks if you want to be more strict
+            if (decodedExpiredToken.Issuer != _authSettings.Value.Iss)
+                return BadRequest("Invalid parameters");
 
             var UniqueUserIndentifier = decodedExpiredToken.Claims.FirstOrDefault(t => t.Type == "UniqueUserIndentifier");
             if (UniqueUserIndentifier == null)
-                return BadRequest("Invalid token");
+                return BadRequest("Invalid parameters");
 
-            var (refreshedToken, refreshedExpiry) = GetToken(UniqueUserIndentifier.Value);
+            var user = _userGetByIdQuery.Execute(Guid.Parse(UniqueUserIndentifier.Value));
+            if (user == null)
+                return BadRequest("Invalid parameters");
+
+            var (refreshedToken, refreshedExpiry) = GetToken(user.Id.ToString());
 
             var auth = new AuthenticationDto()
             {
@@ -65,17 +70,17 @@ namespace DotNetCore2.Controllers.Auth
         /// 
         /// </summary>
         /// <param name="parameters"></param>
-        private AuthenticationDto Authorize(UserLoginDto user)
+        private AuthenticationDto Authorize(UserLoginDto userLogin)
         {
-            var identUser = _userQuery.Execute().FirstOrDefault(u => u.Username == user.Username);
-            if (identUser == null)
+            var user = _userGetAllQuery.Execute().FirstOrDefault(u => u.Username == userLogin.Username);
+            if (user == null)
                 throw new ArgumentException("Invalid parameters");
 
-            var isValidated = CryptoHelperWrapper.VerifyPassword(identUser.Password, user.Password);
+            var isValidated = CryptoHelperWrapper.VerifyPassword(user.Password, user.Password);
             if (!isValidated)
                 throw new ArgumentException("Invalid parameters");
 
-            var (token, expiry) = GetToken(identUser.Id.ToString());
+            var (token, expiry) = GetToken(user.Id.ToString());
 
             return new AuthenticationDto()
             {
@@ -94,13 +99,13 @@ namespace DotNetCore2.Controllers.Auth
                 new Claim("UniqueUserIndentifier", userId)
             };
 
-            var symmetricKeyAsBase64 = _settings.Value.Secret;
+            var symmetricKeyAsBase64 = _authSettings.Value.Secret;
             var keyByteArray = Encoding.ASCII.GetBytes(symmetricKeyAsBase64);
             var signingKey = new SymmetricSecurityKey(keyByteArray);
 
             var jwt = new JwtSecurityToken(
-                issuer: _settings.Value.Iss,
-                audience: _settings.Value.Aud,
+                issuer: _authSettings.Value.Iss,
+                audience: _authSettings.Value.Aud,
                 claims: claims,
                 notBefore: now,
                 expires: now.Add(TimeSpan.FromMinutes(1)),
